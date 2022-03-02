ea7d93ffc74ffa71fef005b809e68ac2daff418f
Vahram Grigoryan <ramgrigoryan@gmail.com>
2022-03-02 10:48:14 +0400

    Logging logic added

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,5 +1,13 @@
+const PLAYER_ATTACK = "PLAYER_ATTACK";
+const PLAYER_STRONG_ATTACK = "PLAYER_STRONG_ATTACK";
+const MONSTER_ATTACK = "MONSTER_ATTACK";
+const PLAYER_HEAL = "PLAYER_HEAL";
+const GAME_OVER = "GAME_OVER";
+
 let healChecker = 0;
 let bonusLife = true;
+let logEntries = [];
+
 defaultSet();
 function defaultSet() {
   let userLifeSet = +prompt("Set level for both sides life!");
@@ -20,23 +28,50 @@ function resetGame() {
 function checkGameOver() {
   if (monsterHealthBar.value == 0 && playerHealthBar.value != 0) {
     alert("Player win!");
+    logLogic(
+      GAME_OVER,
+      "Player win",
+      playerHealthBar.value,
+      monsterHealthBar.value
+    );
     resetGame();
   } else if (monsterHealthBar.value != 0 && playerHealthBar.value == 0) {
+    logLogic(
+      GAME_OVER,
+      "Monster win",
+      playerHealthBar.value,
+      monsterHealthBar.value
+    );
     alert("Monster win!");
     resetGame();
   } else if (playerHealthBar.value == 0 && monsterHealthBar.value === 0) {
     alert("Tie.");
+    logLogic(
+      GAME_OVER,
+      "A DRAW",
+      playerHealthBar.value,
+      monsterHealthBar.value
+    );
     resetGame();
   }
 }
 function moveMaker(moveType) {
   let playerPower;
+  let attackmode;
   if (moveType === "normal") {
     playerPower = Math.round(Math.random() * 12);
+    attackmode = PLAYER_ATTACK;
   } else if (moveType === "strong") {
     playerPower = Math.round(Math.random() * 18);
+    attackmode = PLAYER_STRONG_ATTACK;
   }
   monsterHealthBar.value -= playerPower;
+  logLogic(
+    attackmode,
+    playerPower,
+    playerHealthBar.value,
+    monsterHealthBar.value
+  );
   monsterAttack();
   checkGameOver();
 }
@@ -49,6 +84,12 @@ function monsterAttack() {
     return;
   }
   playerHealthBar.value -= monsterPower;
+  logLogic(
+    MONSTER_ATTACK,
+    monsterPower,
+    playerHealthBar.value,
+    monsterHealthBar.value
+  );
 }
 function attackBtnHandler() {
   moveMaker("normal");
@@ -68,6 +109,12 @@ function healBtnHandler() {
     alert("You can't heal yourself now!");
   } else {
     playerHealthBar.value += healValue;
+    logLogic(
+      PLAYER_HEAL,
+      healValue,
+      playerHealthBar.value,
+      monsterHealthBar.value
+    );
     healChecker++;
   }
   monsterAttack();
@@ -76,6 +123,38 @@ function healBtnHandler() {
 function bonusLogic() {
   playerHealthBar.value;
 }
+function logLogic(event, value, playerHealth, monsterHealth) {
+  let moveCount;
+  if (event === PLAYER_ATTACK || event === PLAYER_STRONG_ATTACK) {
+    moveCount = {
+      event: event,
+      value: value,
+      target: "MONSTER",
+      playerHealth: playerHealth,
+      monsterHealth: monsterHealth,
+    };
+  } else if (event === MONSTER_ATTACK) {
+    moveCount = {
+      event: event,
+      value: value,
+      target: "PLAYER",
+      playerHealth: playerHealth,
+      monsterHealth: monsterHealth,
+    };
+  } else if (event === PLAYER_HEAL || event === GAME_OVER) {
+    moveCount = {
+      event: event,
+      value: value,
+      playerHealth: playerHealth,
+      monsterHealth: monsterHealth,
+    };
+  }
+  logEntries.push(moveCount);
+}
+function logBtnHandler() {
+  console.log(logEntries);
+}
 attackBtn.addEventListener("click", attackBtnHandler);
 strongAttackBtn.addEventListener("click", strongAttackBtnHandler);
 healBtn.addEventListener("click", healBtnHandler);
+logBtn.addEventListener("click", logBtnHandler);
